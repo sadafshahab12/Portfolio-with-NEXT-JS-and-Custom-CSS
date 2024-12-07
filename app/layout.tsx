017b0a83d09888642ce0bd3c5ce8de5a2b8a99d7
Sadaf Shahab <sadafshahab123@yahoo.com>
2024-12-06 19:25:44 -0600

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="/images/favicon.ico.png" type="image/png" />
+      </head>
       <body>
         {children}
 
